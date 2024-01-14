4460b8a4fa7eac3aaa9b015ee67fc8791ef7959e
Subhamoy Biswas <subhamoybiswas636@gmail.com>
2024-01-15 02:04:22 +0530

    (feat): switched to production ready gunicorn server from flask dev server

--- webserver.py
+++ webserver.py
@@ -1,16 +1,25 @@
 from flask import Flask
 from threading import Thread
-#this file is used to host and run your telegram bot 24/7 on a server with a weburl.
+import subprocess
+#this file is used to host and run your telegram bot 24/7 on a webserver.
 #this script is developed by @neo_subhamoy
-#website: https://neosubhamoy.xyz
+#website: https://neosubhamoy.com
 
-app = Flask('')
+app = Flask('Adlinkfly Telegram Bot')
 
 @app.route('/')
 def home():
   return "Adlinkfly Telegram Bot is Running!"
-def run():
-  app.run(host='0.0.0.0', port=8080)
+
+def run_gunicorn():
+    gunicorn_command = [
+        'gunicorn',
+        '-b', '0.0.0.0:8080',
+        '-w', '4',
+        'webserver:app',
+    ]
+    subprocess.Popen(gunicorn_command)
+
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run_gunicorn)
+    t.start()
